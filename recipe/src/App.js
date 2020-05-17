b934d42100b12e7467f8637ba3fe23698aa98785
Charles Ivia <iviacharles@gmail.com>
2020-05-17 11:44:48 +0300

    Added project files

---
+++ recipe/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App = ()=> {
+  const
+  return(
+    <div className="App">
+      <h1>Hello React</h1>
+    </div>
+  );
+}
+
+export default App;

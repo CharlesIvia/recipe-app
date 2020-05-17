37c887d962a891d02faa512d66629b598fb5260c
Charles Ivia <iviacharles@gmail.com>
2020-05-17 15:11:56 +0300

    Added styling logic

--- recipe/src/Recipe.js
+++ recipe/src/Recipe.js
@@ -3,14 +3,15 @@ import style from "./recipe.module.css"
 const Recipe = ({title,calories,image, ingredients})=>{
     return(
         <div className={style.recipe}>
-        <h1>{title}</h1>
-        <p>Calories: {calories}</p>
+        <h1 className={style.title}>{title}</h1>
+        <img className={style.image}  src={image} alt=""/>
+        <h2 className={style.ingredient}>Ingredients</h2>
         <ol>
             {ingredients.map(ingredient => (
                 <li>{ingredient.text}</li>
             ))}
         </ol>
-        <img className={style.image}  src={image} alt=""/>
+        <p className={style.calories}> Calories: {calories}</p>
         </div>
     );
 }

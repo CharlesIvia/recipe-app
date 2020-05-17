a4b4325686ae7add06e6a701bdd62be3e77b2950
Charles Ivia <iviacharles@gmail.com>
2020-05-17 12:07:53 +0300

    Integrated Edamam search recipe Api

--- recipe/src/App.js
+++ recipe/src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 const App = ()=> {
-  const
+  const APP_ID = "a9e37851";
+
+  const APP_KEY = "a388d48a5936ba2b36690638939a4f0f	";
+
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(()=>{
+    getRecipes();
+  }, []);
+
+  const getRecipes = async ()=> {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    
+    console.log(data);
+    setRecipes(data.hits);
+  }
+
   return(
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Search</button>
+      </form>
     </div>
   );
 }

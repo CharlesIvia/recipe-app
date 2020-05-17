d2e50b90c125b16d8973aa4af02e2e79ee3defe9
Charles Ivia <iviacharles@gmail.com>
2020-05-17 15:26:51 +0300

    Updates

---
+++ precache-manifest.f00fb287e070a6bee538d72b4aeb8ebc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ed9785c45d7da60e6ac733137c41099",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "4050d0fb500631eb5878",
+    "url": "/recipe-app/static/css/main.c26ba8f5.chunk.css"
+  },
+  {
+    "revision": "284f2244555fc40838a9",
+    "url": "/recipe-app/static/js/2.2b1aef84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-app/static/js/2.2b1aef84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4050d0fb500631eb5878",
+    "url": "/recipe-app/static/js/main.2453ee7c.chunk.js"
+  },
+  {
+    "revision": "5ced517cbc0f7243ac0d",
+    "url": "/recipe-app/static/js/runtime-main.2010b7c6.js"
+  }
+]);
